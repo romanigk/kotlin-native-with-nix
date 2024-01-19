3f6c14d85494c54c06dc71df754bcd58d65d79d4
Robert Manigk <p1ng0ut@mailbox.org>
2024-01-19 15:50:52 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("multiplatform") version "1.9.22"
+}
+
+repositories {
+    mavenCentral()
+}
+
+kotlin {
+    // macosX64("native") { // on macOS
+    linuxX64("native") { // on Linux
+    // mingwX64("native") { // on Windows
+        binaries {
+            executable()
+        }
+    }
+}
+
+tasks.withType<Wrapper> {
+    gradleVersion = "8.1.1"
+    distributionType = Wrapper.DistributionType.BIN
+}
